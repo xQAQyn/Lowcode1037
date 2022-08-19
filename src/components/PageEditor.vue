2af5c337acafaec496dc1f5da18957c98a38b0aa
Weiyi Chen <wychen325@gmail.com>
2022-08-19 11:28:48 +0800

    feat: render editor with module style support

--- src/components/PageEditor.vue
+++ src/components/PageEditor.vue
@@ -1,56 +1,81 @@
 <template>
-    <div id="edt">
-        <span v-html="render()" ref="editor"></span>
-    </div>
+  <div id="edt" ref="editor">
+    <span v-if="isMounted">
+      <div
+        v-for="mod in modules"
+        :key="mod.id"
+        :id="mod.id"
+        class="module"
+        :style="GetStyleObject(mod)"
+      >
+        <span v-html="GenerateHtml(mod)"></span>
+      </div>
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "PageEditor",
-    methods: {
-        render: function () {
-            const modules = this.$store.state.PageModules;
-            var content = [];
-            for (var i = 0; i < modules.length; i++) {
-                switch (modules[i].type) {
-                    case "p":
-                        content.push(`<p>${modules[i].value}</p>`);
-                        break;
-                    case "a":
-                        content.push(`<a href="${modules[i].url}">${modules[i].value}</a>`);
-                        break;
-                    case "button":
-                        content.push(`<button>${modules[i].value}</button>`);
-                        break;
-                    case "img":
-                        content.push(`<img src="${modules[i].url}"/>`);
-                        break;
-                    case "video":
-                        content.push(`<video src="${modules[i].url}"/>`);
-                        break;
-                }
-            }
-            return content.join("");
-        },
-        op(){
-            console.log(this.$refs.editor.offsetTop)
-        },
-        GetOffsetLeft: function () {
-            console.log(this.$refs.editor.offsetLeft)
-            return this.$refs.editor.offsetLeft;
-        },
-        GetOffsetTop: function () {
-            return this.$refs.editor.offsetTop;
-        },
+  name: "PageEditor",
+  data: function () {
+    return {
+      modules: this.$store.state.PageModules,
+      isMounted: false,
+    };
+  },
+  methods: {
+    op() {
+      console.log(this.$refs.editor.offsetTop);
     },
+    GetOffsetLeft: function () {
+      console.log(this.$refs.editor.offsetLeft);
+      return this.$refs.editor.offsetLeft;
+    },
+    GetOffsetTop: function () {
+      return this.$refs.editor.offsetTop;
+    },
+    GetStyleObject: function (mod) {
+      return {
+        left: mod.style.ToLeft + this.GetOffsetLeft() + "px",
+        top: mod.style.ToTop + this.GetOffsetTop() + "px",
+        width: mod.style.BoxWidth + "px",
+        height: mod.style.BoxHeight + "px",
+        backgroundColor: mod.style.BackgroundColor,
+        color: mod.style.FontColor,
+        fontSize: mod.style.FontSize + "px",
+        fontWeight: mod.style.FontWeight,
+      };
+    },
+    GenerateHtml: function (mod) {
+      switch (mod.type) {
+        case "p":
+          return `<p>${mod.value}</p>`;
+        case "a":
+          return `<a href="${mod.url}">${mod.value}</a>`;
+        case "button":
+          return `<button>${mod.value}</button>`;
+        case "img":
+          return `<img src="${mod.url}"/>`;
+        case "video":
+          return `<video src="${mod.url}"/>`;
+      }
+    },
+  },
+  mounted() {
+    this.isMounted = true;
+  },
 };
 </script>
 
 <style scoped>
 #edt {
-    background-color: grey;
-    flex: 0 0 800px;
-    height: 450px;
-    margin: 200px 10px;
+  background-color: grey;
+  flex: 0 0 800px;
+  height: 450px;
+  margin: 200px 10px;
+}
+
+.module {
+  position: absolute;
 }
 </style>
